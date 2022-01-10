c970c4aae5b78d84e3c4c91cde7d04de74bfc3be
Dylan <dylan.white@netactive.co.za>
2022-01-10 11:17:03 +0200

    tracked ftp approach

---
+++ ftp.py
@@ -0,0 +1,34 @@
+import shutil
+import urllib.request as request
+from contextlib import closing
+#from clint.textui import progress   
+import time
+
+"""
+initial=time.time()
+r=closing(request.urlopen('ftp://ftp-test.telkomadsl.co.za/1Meg-test.file'))
+after=time.time()
+diff=after-initial
+print(str(diff))
+with open('text.txt', 'wb') as f:
+        shutil.copyfileobj(r, f)
+
+"""
+def speedTest():
+    ini=time.time_ns()
+    with closing(request.urlopen('ftp://ftp-test.telkomadsl.co.za/4Meg-test.file')) as r:
+        af=time.time_ns()
+        dif=af-ini
+        dif=dif/(10**9)
+        #print(dif)
+        print("speed= "+str(4/dif))
+        print("it")
+        return dif
+
+mylist=[]
+for i in range(10):
+       mylist.append(speedTest()) 
+avgTime=sum(mylist)/len(mylist)
+avgSpeed=4/avgTime
+print("avg speed= "+str(avgSpeed))
+    
