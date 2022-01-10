095b8a1c4fcdea088b62a64c0c7aae77c596af64
Dylan <dylan.white@netactive.co.za>
2022-01-10 11:44:36 +0200

    removed some excess

--- Speedtest Python API Approach/hello_speedtest.py
+++ Speedtest Python API Approach/hello_speedtest.py
@@ -11,13 +11,9 @@ threads = None
 s = speedtest.Speedtest()
 s.get_servers(servers)
 s.get_best_server()
-s.download(threads=threads)
-s.upload(threads=threads)
-s.results.share()
 
 for i in range(10):
     s.download(threads=threads)
-    s.results.share()
     results_dict = s.results.dict()
     print(results_dict["download"]/(10**6))
 
